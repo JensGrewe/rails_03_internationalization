99aecc2e2479c6a9bbe4c33d9e3dd72971803d3f
B Kleinen <drblinken@gmail.com>
2013-06-07 00:16:31 +0200

    Selecting the locale 1: setting default locale

---
+++ config/initializers/I18n.rb
@@ -0,0 +1,11 @@
+#encoding: utf-8
+#set the default locale for the application in the
+#I18n (Internationalization) Module
+I18n.default_locale = :de
+#define associations between display names and locale names
+#with a method of entering non available characters
+LANGUAGES = [
+['English', 'en'],
+["Espa&ntilde;ol".html_safe, 'es'],
+['Deutsch','de']
+]
